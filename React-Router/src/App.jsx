071c823be5a887fbc417f8f230075976099bd45c
saray leal <sarayleal2022@gmail.com>
2023-02-21 15:25:45 +0100

    React-Router

---
+++ React-Router/src/App.jsx
@@ -0,0 +1,25 @@
+import { NavLink, Outlet } from 'react-router-dom'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <header className="header">
+        <h1>React Router v6 🧪</h1>
+      </header>
+      <div>
+        <nav>
+          <NavLink to="">Home</NavLink>
+          <NavLink to="heroes">Heroes</NavLink>
+          <NavLink to="about">About</NavLink>
+        </nav>
+        <main>
+          <Outlet />
+        </main>
+      </div>
+    </div>
+  )
+}
+
+export default App
