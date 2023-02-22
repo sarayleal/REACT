669bad6455474d30768496b7c8e951339adc9287
saray leal <sarayleal2022@gmail.com>
2023-02-22 20:51:11 +0100

    useStateOne

--- useStateOne/src/App.jsx
+++ useStateOne/src/App.jsx
@@ -1,8 +1,14 @@
 import './App.css'
-import {MyState} from "./components/MyState";
+import { MyState } from "./components/MyState.jsx";
+import MiniCodeObjectState from './components/ObjectState';
 
 const App = () => {
-  return <MyState />;
+  return (
+ <div className='App'>
+      <MyState />
+      <MiniCodeObjectState />
+ </div>
+  )
 };
 
 export default App
